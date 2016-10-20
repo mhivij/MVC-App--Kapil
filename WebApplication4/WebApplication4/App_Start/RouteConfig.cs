552b673c9b13278e56d899b02d26fa0e4e77b43a
kapilsharma46 <kapil.sharma@cygrp.com>
2016-10-20 18:44:18 +0530

    First Commit

---
+++ WebApplication4/WebApplication4/App_Start/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication4
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Customer", action = "GetData", id = UrlParameter.Optional }
+            //);
+            routes.MapRoute(
+                name: "Customer",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Customer", action = "InsertData", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4/Controllers/CustomerController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
+using WebApplication4.Models;
+using System.Text.RegularExpressions;
+
+namespace WebApplication4.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private Models.Training_Orders_Engine_SandboxEntities ent = new Models.Training_Orders_Engine_SandboxEntities();
+       
+        public ActionResult GetData()
+        {            
+            
+            var ent = new Models.Training_Orders_Engine_SandboxEntities();
+            return View(ent.Customers.ToList());
+        }
+        
+        public ActionResult InsertData()
+        {          
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertData(Customer cust)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("InsertData");
+            }
+            
+                ent.Customers.Add(cust);
+                ent.SaveChanges();
+                return RedirectToAction("GetData");
+        
+            
+            
+        }
+        
+        public ActionResult Delete(int id)
+        {           
+            Customer cust = ent.Customers.Find(id);
+            if(cust == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cust);
+        }
+
+        [HttpPost][ActionName("Delete")]
+        public ActionResult DeleteData(int id)
+        {
+            Customer cust = ent.Customers.Find(id);
+            ent.Customers.Remove(cust);
+            ent.SaveChanges();
+            return RedirectToAction("GetData");
+        }
+        public ActionResult Edit(int id)
+        {
+            Customer cust = ent.Customers.Find(id);
+            return View(cust);
+        }
+        [HttpPost][ActionName("Edit")]
+        public ActionResult EditData(int id, Customer cust)
+        {
+            Customer tempCust = ent.Customers.Find(id);
+            ent.Customers.Remove(tempCust);
+            ent.Customers.Add(cust);
+            ent.SaveChanges();
+            return RedirectToAction("GetData");
+        }
+
+        //public ActionResult DropDownList()
+        //{
+        //    Customer cust = new Customer();
+        //    cust.CountryList.
+        //}
+
+    }
+}
